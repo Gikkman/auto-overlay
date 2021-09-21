4b3c846e9fc423d4da798f86d3ed52341de006d7
gikkman <gikk@gikkman.com>
2021-09-21 14:47:27 +0200

    Filetree scanner

---
+++ src/filetree-scanner.ts
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+
+type FileEntity = {name: string, absolutePath: string, type: 'file'|'folder'}
+type FolderStub = {name: string, absolutePath: string, type: 'folder'};
+type Folder = FolderStub & {children: FileEntity[]};
+
+
+export function filetreeScanner(folderRelativePath: string): Folder {
+    const cwd = process.cwd();
+    const absolutePath = path.join(cwd, folderRelativePath);
+    const name = path.basename(absolutePath);
+    if( !fs.existsSync(absolutePath) ) throw `No folder exists for value ${folderRelativePath} at location ${absolutePath}.`;
+
+    return filetreeScannerRecursive( {name, absolutePath, type: 'folder'});
+}
+
+function filetreeScannerRecursive(folder: FolderStub): Folder {
+    const children = fs.readdirSync(folder.absolutePath, { withFileTypes: true} )
+        .filter( obj => obj.isDirectory() || obj.isFile() || obj.isSymbolicLink() )
+        .map( obj => {
+            const name = obj.name;
+            const absolutePath = path.join(folder.absolutePath, obj.name);
+            const type = obj.isFile() ? 'file' : 'folder';
+            const entity: FileEntity = {name, absolutePath, type};
+            return entity;
+        })
+        .map(obj => {
+            if(obj.type === "file") {
+                return obj;
+            }
+            else {
+                const folder = obj as FolderStub;
+                return filetreeScannerRecursive(folder);
+            }
+        });
+    return {...folder, children};
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { HELLO } from "./module";
 import { readFile, readProperties } from "./read-file";
+import { filetreeScanner } from "./filetree-scanner";
 
 console.log(HELLO);
 
@@ -7,3 +8,9 @@ console.log( readFile("README.md") );
 
 const props = readProperties("auto-overlay.properties");
 console.log( props.getAllProperties() );
+
+const inputFolder = props.get('input.folder');
+if(inputFolder === null) process.exit(1);
+
+const fileTree = filetreeScanner(inputFolder.toString());
+console.log(JSON.stringify(fileTree, null, 2));
